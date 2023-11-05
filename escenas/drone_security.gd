72ce8b8ffde19d7279300dca35a39b1ce6548495
Ema Galeas <ema.castellanos@unitec.edu>
2023-11-04 22:26:55 -0600

    lvl 2 terminado

--- escenas/drone_security.gd
+++ escenas/drone_security.gd
@@ -1,3 +1,42 @@
 extends CharacterBody2D
 
+const speed=100
+var player=null
+var playerchase=false
+var toques=0
 
+func _physics_process(delta):
+	if playerchase:
+		velocity = position.direction_to(player.position) * speed
+	else:
+		velocity = Vector2.ZERO  # Establece la velocidad en cero si no hay jugador
+	move_and_slide()
+
+func _on_detect_body_entered(body):
+	player=body
+	playerchase=true
+	$anime.play("impacto")
+
+
+func _on_detect_body_exited(body):
+	player=null
+	playerchase=false
+	$anime.play("default")
+
+
+func _on_impacto_dentro_body_entered(body):
+	if toques==3:
+		get_tree().change_scene_to_file("res://escenas/menu.tscn")
+	pass
+	
+
+
+func _on_audiodrone_finished():
+	$audiodrone.play()
+
+
+func _on_impacto_dentro_body_exited(body):
+	toques=toques+1
+	if toques==3:
+		get_tree().change_scene_to_file("res://escenas/menu.tscn")
+	pass
--- escenas/level_2.gd
+++ escenas/level_2.gd
@@ -1,4 +1,7 @@
 extends Node2D
 
-func _on_fondolvl_finished():
-	$sonido.play()
+var label=null
+func _process(delta):
+	if label:
+		label.position.x = position.x
+		get_node("label").text = "intas"
