4bf9c0f6b5ea15c3f13a1547b4a8726fabf9fae5
Ema Galeas <ema.castellanos@unitec.edu>
2023-12-07 10:27:29 -0600

    nivel 2 mapa y delorian

--- escenas/delorianS.gd
+++ escenas/delorianS.gd
@@ -1,6 +1,14 @@
 extends Area2D
 
 
+
+
+func _on_body_entered(body):
+	body.hide()
+	$del.play("default")
+	$sdd.play()
+
+
 func _on_del_animation_finished():
 	if VariablesGlobales.enFinal==false:
 		VariablesGlobales.videoLVL2=0
@@ -8,10 +16,3 @@ func _on_del_animation_finished():
 	else:
 		VariablesGlobales.finalVista()
 		get_tree().change_scene_to_file("res://escenas/story_lvl2.tscn")
-
-
-func _on_body_entered(body):
-	body.hide()
-	$del.play("default")
-	$sdd.play()
-	
